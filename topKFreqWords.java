beb1a7141c2e3d01a5e634f11c0e9b243063d011
nandhinik17 <118190100+nandhinik17@users.noreply.github.com>
2025-10-22 12:52:03 +0530

    Create topKFreqWords.java

---
+++ topKFreqWords.java
@@ -0,0 +1,46 @@
+/*Given an array of strings words and an integer k, return the k most frequent strings.
+
+Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
+
+ 
+
+Example 1:
+
+Input: words = ["i","love","leetcode","i","love","coding"], k = 2
+Output: ["i","love"]
+Explanation: "i" and "love" are the two most frequent words.
+Note that "i" comes before "love" due to a lower alphabetical order.
+Example 2:
+
+Input: words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4
+Output: ["the","is","sunny","day"]
+Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the number of occurrence being 4, 3, 2 and 1 respectively.*/
+
+
+
+class topKFreqWords {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new TreeMap<String, Integer>();
+        List<String> resList = new ArrayList<String>();
+        for(String s: words){
+            if(!map.containsKey(s)){
+                map.put(s, 1);
+            }
+            else{
+                map.put(s, map.get(s)+1);
+            }
+        }
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((a, b)-> b.getValue()-a.getValue());
+        
+        for(Map.Entry<String, Integer> e: list){
+            resList.add(e.getKey());
+            k--;
+            if(k==0){
+                break;
+            }
+        }
+
+        return resList;
+    }
+}
